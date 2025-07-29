9034d32ccdc8d0702a7fac462ed8c24250df7330
wookhyung <qpflapffhs76@gmail.com>
2025-07-29 16:53:22 +0900

    feat: 포스트 필터링 및 요약 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,20 +2,21 @@ import { allPosts } from 'contentlayer/generated';
 import Link from 'next/link';
 
 export default function Page() {
-  const sortedPosts = allPosts.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
-  );
+  const posts = allPosts
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .filter((post) => !post.draft);
 
   return (
     <>
-      {sortedPosts.map((post) => (
+      {posts.map((post) => (
         <Link
           href={`/blog/${post.slug}`}
           className="border-b border-gray-200 py-3 px-2 hover:bg-gray-50 flex flex-col gap-2"
           key={post._id}
         >
           <h2 className="text-lg font-bold">{post.title}</h2>
-          <p className="text-sm text-gray-500 mb-2">
+          <p className="text-sm text-gray-500 mb-2">{post.summary}</p>
+          <p className="text-xs text-gray-500">
             {new Date(post.date).toLocaleDateString('ko-KR', {
               year: 'numeric',
               month: 'long',
