7b4ba868b8e5e47d22dc9051557e7fdda4e3e32d
wookhyung <qpflapffhs76@gmail.com>
2025-09-24 21:52:05 +0900

    refactor: remove summary and tags from posts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -18,16 +18,6 @@ export const TechPost = defineDocumentType(() => ({
       description: 'date of the post',
       required: true,
     },
-    summary: {
-      type: 'string',
-      description: 'summary of the post',
-      required: true,
-    },
-    tags: {
-      type: 'list',
-      of: { type: 'string' },
-      required: false,
-    },
     draft: {
       type: 'boolean',
       description: 'draft of the post',
@@ -65,16 +55,6 @@ export const NotesPost = defineDocumentType(() => ({
       type: 'date',
       description: 'date of the post',
       required: true,
-    },
-    summary: {
-      type: 'string',
-      description: 'summary of the post',
-      required: true,
-    },
-    tags: {
-      type: 'list',
-      of: { type: 'string' },
-      required: false,
     },
     draft: {
       type: 'boolean',
